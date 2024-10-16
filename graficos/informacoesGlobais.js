5b758283494b4e2516253229f247539774ba580a
BECHER10 <165160423+BECHER10@users.noreply.github.com>
2024-10-16 11:00:40 -0300

    .

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -1,13 +1,13 @@
-const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
+const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json&#39;
 
 async function vizualizarInformacoesGlobais() {
-    const res = await fetch (url)
-    const dados = await res.json()
-    console.log(dados);
+const res = await fetch(url)
+const dados = await res.json()
+console.log(dados);
 const paragrafo = document.createElement('p')
 paragrafo.classList.add('graficos-container__texto')
 
-paragrafo.innerHTML = `Você sabia que o mundo tem <span> $(dados.total_pessoas_mundo)</span> de pessoas e que aproximadamente ${dados.total_pessoas_conectadas} estão conectados em algumas rede social e passam em media <spam>${dados.tempo_medio}</span> horas conectadas.`
+paragrafo.innerHTML = `Você sabia que o mundo tem <span>${dados.total_pessoas_mundo}</span> de pessoas e que aproximadamente <span>${dados.total_pessoas_conectadas}</span> estão conectadas em alguma rede social e passam em média <span>${dados.tempo_medio}</span> horas conectadas.`
 console.log(paragrafo)
 
 const container = document.getElementById('graficos-container')
